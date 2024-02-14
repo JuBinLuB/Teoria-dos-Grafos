c80598fc1b62bef4544b1b635ef358bd10c0be3c
Alexssander Fernandes <alexssander84@hotmail.com>
2024-02-14 18:11:25 -0300

    modified:   "Trabalho Pr\303\241tico 01/images/possible_path.bmp" 	modified:   "Trabalho Pr\303\241tico 01/src/__pycache__/draw_path.cpython-312.pyc" 	modified:   "Trabalho Pr\303\241tico 01/src/draw_path.py"

--- Trabalho Prático 01/src/draw_path.py
+++ Trabalho Prático 01/src/draw_path.py
@@ -1,21 +1,22 @@
 from PIL import Image
 
 def draw_path(path: list, image_name: str) -> None:
-    """
-    Draw the specified path on the image and save the resulting image.
+  """
+  Draw the specified path on the image and save the resulting image.
 
-    Parameters:
-    - path: List of coordinates representing the path to be drawn.
-    - image_name: The name of the image file to be read and modified.
+  Parameters:
+  - path: List of coordinates representing the path to be drawn.
+  - image_name: The name of the image file to be read and modified.
 
-    Returns:
-    - None
-    """
-    image = Image.open(image_name).convert("RGB")
-    pixels = image.load()
-    for v in path:
-      x, y = v
+  Returns:
+  - None
+  """
+  image = Image.open(image_name).convert("RGB")
+  pixels = image.load()
+  
+  for x, y in path:
+    pixel_color = image.getpixel((x, y))
+    if pixel_color not in [(255, 0, 0), (0, 255, 0)]:
       pixels[x, y] = (0, 0, 255)
-    # Save the resulting image with the drawn path.
-    image.save("E:\Pandora's Box\Documents\Faculdade\Teoria dos Grafos\Trabalho Prático 01\images\possible_path.bmp")
-    
+  # Save the resulting image with the drawn path.
+  image.save("E:\Pandora's Box\Documents\Faculdade\Teoria dos Grafos\Trabalho Prático 01\images\possible_path.bmp")
